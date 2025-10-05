cf3fdf9e1492b8a16989ef9dc3a4820d7fb64023
GSNSAMPATH <sumalnadira123@gmail.com>
2025-10-06 04:33:04 +0530

    first commit

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://www.villasathkara.com",
   generateRobotsTxt: true,
   sitemapSize: 7000,
   exclude: ["/api/*", "/admin/*", "/thank-you"], // optional exclusion
