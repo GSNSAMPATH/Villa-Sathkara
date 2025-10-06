23a5cbc8a1eb42afdd1699b2be1b1577b06e6257
GSNSAMPATH <sumalnadira123@gmail.com>
2025-10-06 11:46:02 +0530

    first commit

--- src/app/about/about.tsx
+++ src/app/about/about.tsx
@@ -40,7 +40,7 @@ export default function About() {
             alt="Villa Sathkara"
             width={1920}
             height={1080}
-            priority
+            quality={80}
             className="inset-0 h-[40vh] sm:h-[60vh] lg:h-[80vh] object-cover transition-all duration-1000 rounded-2xl"
           />
 
